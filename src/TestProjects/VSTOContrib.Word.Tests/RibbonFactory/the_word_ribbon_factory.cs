dfd18dcea39906684df59fde1599f35b176df4a1
JakeGinnivan <none@none>
2011-05-30 20:05:13 +0800

    Added test showing how to get around exception when mocking Word interfaces Updated to nSubstitute 1.1

---
+++ src/TestProjects/VSTOContrib.Word.Tests/RibbonFactory/the_word_ribbon_factory.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.InteropServices;
+using Castle.DynamicProxy.Generators;
+using Microsoft.Office.Interop.Word;
+using NSubstitute;
+using VSTOContrib.Word.RibbonFactory;
+using Xunit;
+
+namespace VSTOContrib.Word.Tests.RibbonFactory
+{
+    public class the_word_ribbon_factory
+    {
+        private readonly Application _application;
+
+        public the_word_ribbon_factory()
+        {
+            AttributesToAvoidReplicating.Add<MarshalAsAttribute>();
+            _application = Substitute.For<Application>();
+        }
+
+        [Fact]
+        public void can_initialise()
+        {
+            // arrange
+            WordRibbonFactory.SetApplication(_application);
+
+            // act
+
+            // assert
+
+        }
+    }
+}
