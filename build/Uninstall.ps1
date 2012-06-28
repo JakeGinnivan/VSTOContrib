3be129343b0005078c4df0617327502df4fa50bd
JakeGinnivan <jake@ginnivan.net>
2012-06-28 11:13:56 +0800

    Trying to fix powershell script not running

--- build/Uninstall.ps1
+++
@@ -1,4 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-$project.Object.References | Where-Object -FilterScript { $_.Name.StartsWith('VSTOContrib') } | ForEach-Object { $_.Remove() }
-$project.Save()
