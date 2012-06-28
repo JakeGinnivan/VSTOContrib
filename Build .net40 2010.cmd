42858e809552fe33900278008dd1d4401686f8e0
JakeGinnivan <jake@ginnivan.net>
2012-06-28 11:10:21 +0800

    Fixed delete artifacts dir

--- Build .net40 2010.cmd
+++ Build .net40 2010.cmd
@@ -2,7 +2,7 @@
 call "%VS100COMNTOOLS%vsvars32.bat"
 mkdir .\build\log\
 
-rmdir .\build\Artifacts\
+rmdir .\build\Artifacts\ /s /q
 
 msbuild.exe /ToolsVersion:4.0 /p:Configuration=Release /p:Platform=AnyCPU /p:TargetFrameworkVersion=v4.0 /p:TargetOfficeVersion=2010 /l:FileLogger,Microsoft.Build.Engine;logfile=.\build\log\NET40_2010.log;verbosity=diagnostic "VSTO Contrib.msbuild" 
 if %errorlevel% neq 0 goto end
