a193b17594684294c6268091ddecfacaf737f73e
Jake Ginnivan <jake.ginnivan@readify.net>
2013-07-09 12:20:14 +0800

    Added PowerPoint sample application

--- src/Samples/PowerPoint/AddTextAddin/Properties/AssemblyInfo.cs
+++ src/Samples/PowerPoint/AddTextAddin/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VSTOContrib.Work.Quickstart")]
+[assembly: AssemblyTitle("AddTextAddin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VSTOContrib.Work.Quickstart")]
+[assembly: AssemblyProduct("AddTextAddin")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +21,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3bb871d5-2b60-4037-9816-e246eb87532d")]
+[assembly: Guid("aaef06d5-e0af-4542-9ee5-7deacece7c72")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
---
+++ src/Samples/PowerPoint/AddTextAddin/ThisAddIn.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows;
+using AddTextAddin.Core;
+using Microsoft.Office.Tools;
+using VSTOContrib.Core;
+using VSTOContrib.Core.RibbonFactory;
+using VSTOContrib.PowerPoint.RibbonFactory;
+using Office = Microsoft.Office.Core;
+
+namespace AddTextAddin
+{
+    public partial class ThisAddIn
+    {
+        protected override Office.IRibbonExtensibility CreateRibbonExtensibilityObject()
+	{
+		//Required for WPF support
+        if (System.Windows.Application.Current == null)
+            new Application { ShutdownMode = ShutdownMode.OnExplicitShutdown };
+
+		var assemblyContainingViewModels = typeof (PresentationViewModel).Assembly; // This should be the assembly containing all your VSTOContrib viewmodels
+		return new PowerPointRibbonFactory(new DefaultViewModelFactory(), new Lazy<CustomTaskPaneCollection>(() => CustomTaskPanes), Globals.Factory, assemblyContainingViewModels);
+	}
+
+        private void ThisAddIn_Startup(object sender, EventArgs e)
+        {
+            RibbonFactory.Current.SetApplication(Application, this);
+        }
+
+        private void ThisAddIn_Shutdown(object sender, EventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            Startup += ThisAddIn_Startup;
+            Shutdown += ThisAddIn_Shutdown;
+        }
+
+        #endregion
+    }
+}
