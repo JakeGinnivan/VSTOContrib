1578fb6e20011c0e59961227dec43c0164b35586
Jake Ginnivan <jake.ginnivan@readify.net>
2014-05-26 16:24:32 +0100

    Merged QuoteGeneratorAddin.Core into main addin project

---
+++ src/Samples/Excel/QuoteGeneratorAddin/AddinModule.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+using QuoteGeneratorAddin.Core.OfficeContexts;
+using VSTOContrib.Autofac;
+
+namespace QuoteGeneratorAddin.Core
+{
+    public class AddinModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterRibbonViewModels(typeof(AddinModule).Assembly);
+            builder.RegisterType<QuotesService>().As<IQuotesService>().SingleInstance();
+        }
+    }
+}
